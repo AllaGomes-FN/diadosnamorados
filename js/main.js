4022f0a8f1ad5b6fefba3a77568966be4a798a5f
AllaGomes-FN <allanga1999@gmail.com>
2020-06-11 16:53:01 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.your-class').slick({
+    autoplaySpeed: 8000,
+    speed: 250,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear',
+    pauseOnFocus: false,
+    pauseOnHover: false,
+});
